10ca454928d327070db169eee8b1203ca21a3928
kolmit <gazord_30@hotmail.fr>
2017-09-11 18:27:43 +0200

    Calculatrice

---
+++ Calculatrice-v1/src/fr/esisar/calculatrice/Calculateur.java
@@ -0,0 +1,20 @@
+package fr.esisar.calculatrice;
+
+public class Calculateur {
+	
+	
+	public void ajouter(Integer operande1, Integer operande2) {
+		
+	}
+	public void soustraire(Integer operande1, Integer operande2) {
+			
+	}
+	
+	public void multiplier(Integer operande1, Integer operande2) {
+		
+	}
+	
+	public void diviser(Integer operande1, Integer operande2) {
+		
+	}
+}
